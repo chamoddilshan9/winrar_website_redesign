97f1eb8993ed426aa2f4a815059fdbb0bcf6ea12
Chamod Dilshan <chamoddilshancontact@gmail.com>
2025-04-14 13:47:51 +0530

    image domains added in next config file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.sftcdn.net','mac-cdn.softpedia.com','www.win-rar.com'],
+  },
+}
+
+module.exports = nextConfig 
